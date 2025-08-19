f9a1c0e212fd0aa9bfd7fe12a7d97b6f36055437
Ali <alibayaa07@gmail.com>
2025-08-19 17:43:18 -0400

    optimized and added more features

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ damage = 25
 monster_damage = 10
 
 def menu():
-    print("Escape The Maze")
+    print("The Maze")
     print("1. Start Game")
     print("2. Exit")
 
@@ -19,37 +19,60 @@ def start_game():
         movement = input("Left, Right, or Forward?: ")
         if movement.lower() == "left":
             print("You moved left.")
-            random_event = random.randint(1, 10)
-            if random_event % 2 != 0:
-                battle()
-                y += 1
-            
-            else:
-                print("You advanced through the maze.")
-                y += 1    
+               
         elif movement.lower() == "right":
             print("You moved right.")
-            random_event = random.randint(1, 11)
-            if random_event % 2 != 0:
-                battle()
-                y += 1
-            else:
-                print("You advanced through the maze.")
-                y += 1
+           
         elif movement.lower() == "forward":
             print("You moved forward.")
-            random_event = random.randint(1, 11)
-            if random_event % 2 != 0:
+        
+        else:
+            print("Invalid direction.")
+            print("In your confusion, you accidentally walk into a wall and take 10 damage.")
+            global health
+            health -= 10
+            print(f"You have {health} health remaining")
+            
+
+        random_event = random.randint(1, 10)
+        if random_event % 2 != 0:
                 battle()
                 y += 1
-            else:
-                print("You advanced through the maze.")      
+        elif random_event == 2:
+                loot()
+                print("You advanced through the maze.")
                 y += 1
+        elif random_event == 6:
+             print("You encountered a dead end and had to turn back.")
+             y -= 1
         else:
-            print("Invalid direction, please try again.")
-            start_game()
-    if y >= 10:
-        print("Congratulations! You escaped the maze!")   
+                print("You advanced through the maze.")
+                y += 1 
+        
+    print("Congratulations! You escaped the maze!")   
+
+def fight():
+    monster_health = 75
+    global health
+    while monster_health > 0 and health > 0:
+            time.sleep(2)
+            print("You attack the monster")
+            time.sleep(3)
+            monster_health -= damage
+            print(f"You dealt {damage} damage to the monster. Monster health is now {monster_health}.")
+            time.sleep(2)
+            print("The monster attacks")
+            time.sleep(3) 
+            health -= monster_damage
+            print(f"The monster dealt {monster_damage} damage. You have {health} remaining")
+    if monster_health <= 0:
+            print("The monster has been defeated!")
+            print(f"You won with {health} health remaining and advanced through the maze!")
+    elif health <= 0:
+            print("You have been defeated.")
+            print("Game Over")
+            sys.exit()
+
 
 def battle():
     
@@ -59,54 +82,40 @@ def battle():
     print("2. Run")
     action = input("Please select an action: ") 
     if action == "1":
-        monster_health = 75
-        while monster_health > 0:
-            time.sleep(2)
-            print("You attack the monster")
-            time.sleep(3)
-            monster_health -= damage
-            print(f"You dealt {damage} damage to the monster. Monster health is now {monster_health}.")
-            time.sleep(2)
-            print("The monster attacks")
-            time.sleep(3)
-            global health 
-            health -= monster_damage
-            print(f"The monster dealt {monster_damage} damage. You have {health} remaining")
-        if monster_health <= 0:
-            print("The monster has been defeated!") 
-            print (f"You won with {health} health remaining")
-        elif health <= 0:
-            print("You have been defeated.")
-            print("Game Over")
-            sys.exit()
+        fight()
     elif action == "2":
         flee = random.randint(1, 3)
         if flee == 2:
             print("You fled the battle and advanced through the maze.")
         else:
             print("You failed to flee the monster, prepare for battle!")
-            monster_health = 75
-            while monster_health > 0 and health > 0:
-                time.sleep(2)
-                print("You attack the monster")
-                time.sleep(3)
-                monster_health -= damage
-                print(f"You dealt {damage} damage to the monster. Monster health is now {monster_health}.")
-                time.sleep(2)
-                print("The monster attacks")
-                time.sleep(3)
-                health -= monster_damage
-                print(f"The monster dealt {monster_damage} damage. You have {health} remaining")
-            if monster_health <= 0:
-                print("The monster has been defeated!") 
-                print (f"You won with {health} health remaining")
-            elif health <= 0:
-                print("You have been defeated.")
-                print("Game Over")
-                sys.exit()
+            fight()
     else:
         print("Invalid action, please try again.")
         battle()
+
+def loot():
+    random_loot = random.randint(1, 2)
+    if random_loot == 1:
+         print("You stumble upon a chest")
+         time.sleep(2)
+         print("You open it and find a health potion!")
+         print("You drink it and restore 20 health.")
+         global health
+         health += 20
+         if health > 100:
+             health = 100
+         time.sleep(2)
+         print(f"You now have {health} health.")
+    elif random_loot == 2:
+        print("You bump into a knights skeleton.")
+        time.sleep(2)
+        print("You rummage through his belingings.")
+        time.sleep(3)
+        print("You find a sword and now deal 50 damage")
+        global damage
+        damage  = 50
+         
 
 def exit():
     print("Come back and play some time soon!")
